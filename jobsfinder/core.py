f0d7713dc4ff5d6b0715605ab9e45dfdb266a0d6
Kris <krisztiankovacs@fastmail.com>
2024-09-04 12:03:00 +0100

    add md conversion

--- jobsfinder/core.py
+++ jobsfinder/core.py
@@ -1,7 +1,10 @@
 import asyncio
+import re
 from pathlib import Path
 
 import tqdm.asyncio
+from bs4 import BeautifulSoup
+from markdownify import markdownify
 from openai import AsyncClient
 from playwright.async_api import async_playwright
 
@@ -79,3 +82,35 @@ async def simple_gpt(system_msg, user_msg, schema, temperature=0):
             print(err)
             await asyncio.sleep(20)
     raise ValueError("5 iterations did not succeed!")
+
+
+def replace_empty_newlines(text):
+    return re.sub(r"(\n{4,})", "\n\n\n", text)
+
+
+def html2md(html: str | None):
+    if html is None:
+        return None
+
+    soup = BeautifulSoup(html, "html.parser")
+
+    try:
+        # Noticed this issue in some of the first datapoints.
+        # We can also do more cleaning if necessary, but not need to over-optimize this
+        # before doing a proper markdown-converter comparison
+        for noscript in soup.find_all("noscript"):
+            noscript.decompose()
+
+        # Remove all attributes except for alt tags for images (so we don't have bs64 encoded long strings)
+        for tag in soup.find_all(["img", "video", "svg"]):
+            attributes = {
+                key: value for key, value in tag.attrs.items() if key == "alt"
+            }
+            tag.attrs = attributes
+
+        cleaned_html = str(soup)
+        md = markdownify(cleaned_html)
+        return replace_empty_newlines(md)
+    except Exception as e:
+        print(e)
+        return None
